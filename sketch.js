4c4b5d53052c39d5f501eb69ba88154eec26a192
Animal27 <73412410+Animal27@users.noreply.github.com>
2021-09-05 14:27:43 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var spacecraft, iss;
+var spacecraft_IMG, iis_IMG
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+
+  drawSprites();
+}
